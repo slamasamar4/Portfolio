d94d1bfa2a2d1261c7a4c686253a1b52ec1f8e3a
slamasamar4 <slamasamar430@gmail.com>
2024-11-04 12:51:13 +0100

    updatte

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FiTwitter, FiGithub, FiLinkedin} from "react-icons/fi";
+
+const Social = () => {
+    return (
+        <div className="home__social">
+           
+        
+            <a href="https://github.com/slamasamar4" className="slamasamar4" target="_blank">
+                <FiGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/samar-slama430/" className="samar-slama430/" target="_blank">
+                <FiLinkedin />
+            </a>
+        </div> 
+    ); 
+}
+
+export default Social;
