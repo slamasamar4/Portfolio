acd10b477a53c5e6108af1de07c51534f8251dad
slamasamar430 <slamasamar430@gmail.com~>
2024-11-26 10:28:30 +0100

    update

---
+++ src/components/projects/siaziz.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Siaziz = () => {
+    return (
+        <div>
+            <h1>Project 1</h1>
+            <p>Project 1 description</p>
+            
+        </div>
+    );
+}
+
+export default Siaziz;
